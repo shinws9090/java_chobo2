07ebe8e516b2737da1e164558e4c67168216b08d
ShinBG <shinws9090@naver.com>
2021-02-25 09:08:25 +0900

    File 예제작성

--- src/java_chobo2/ch15/FileEx2.java
+++ src/java_chobo2/ch15/FileEx2.java
@@ -1,6 +1,7 @@
 package java_chobo2.ch15;
 
 import java.io.File;
+import java.util.ArrayList;
 
 import javax.swing.JFileChooser;
 
@@ -18,8 +19,37 @@ public class FileEx2 {
 			System.out.println(file.isDirectory() ? "["+file.getName()+"]" : file.getName());
 		}
 		
+		printFileList(f);
+		
 	}
 	
+	private static void printFileList(File f) {
+		System.out.println(f.getAbsolutePath()+"디렉토리");
+		File[] files = f.listFiles();
+		ArrayList subDir = new ArrayList();
+		
+		for(int i =0; i <files.length;i++) {
+			String filename = files[i].getName();
+			if(files[i].isDirectory()) {
+				filename = "["+filename+"]";
+				subDir.add(i+"");
+			}
+			System.out.println(filename);
+		}
+		
+		int dirNum = subDir.size();
+		int fileNum = files.length-dirNum;
+		
+		System.out.println(fileNum + "개의 파일"+dirNum+"개의 디랙토리");
+		System.out.println();
+		
+		for(int i = 0; i <subDir.size();i++) {
+			int index = Integer.parseInt((String)subDir.get(i));
+			printFileList(files[index]);
+		}
+		
+	}
+
 	public static File getSearchFile() { //자바 파일 검색창으로 파일 선택하는법
 		JFileChooser jfc = new JFileChooser(System.getProperty("user.dir"));
 		jfc.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);//차죠 모드 변경
